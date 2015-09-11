e2be6f6c9c1fab9e92a77060289b57d9added054
Hiroaki Nakamura <hnakamur@gmail.com>
2015-09-11 11:06:36 +0900

    Modify gulp task to create sourcemaps
    See https://babeljs.io/docs/setup/#gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
-var src_dir = './es6/**/*.js'
+var sourcemaps = require("gulp-sourcemaps");
+var concat = require("gulp-concat");
+
+var srcFiles = './es6/**/*.js'
 gulp.task('default', function () {
-  return gulp.src(src_dir)
+  return gulp.src(srcFiles)
+    .pipe(sourcemaps.init())
     .pipe(babel())
+    .pipe(concat("main.js"))
+    .pipe(sourcemaps.write("."))
     .pipe(gulp.dest('js'));
 });
 gulp.task('watch', function(){
-    gulp.watch(src_dir, ['default']);
+  gulp.watch(srcFiles, ['default']);
 });
